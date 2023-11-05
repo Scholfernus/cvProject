2c86c032d4648de031351b8fca5abb3c13353d0f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-05 16:24:58 +0100

    Nowy Dto Education

---
+++ src/main/java/com/example/cvproject/dto/EducationDto.java
@@ -0,0 +1,11 @@
+package com.example.cvproject.dto;
+
+import lombok.Data;
+
+@Data
+public class EducationDto {
+    private String education;
+    private String technology;
+    private String description;
+    private Double rank;
+}

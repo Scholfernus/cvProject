3f07cbc45f887f4833697a684d6d257ba48d8add
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-05 16:25:22 +0100

    Nowy Model Education

---
+++ src/main/java/com/example/cvproject/model/EducationModel.java
@@ -0,0 +1,20 @@
+package com.example.cvproject.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class EducationModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String education;
+    private String technology;
+    private String description;
+    private Double rank;
+}

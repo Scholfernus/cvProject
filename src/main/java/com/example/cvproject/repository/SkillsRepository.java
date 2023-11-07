7657183c78e853e52148a06c665e25e1866d2433
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-07 22:01:27 +0100

    Nowe repozytorium

---
+++ src/main/java/com/example/cvproject/repository/SkillsRepository.java
@@ -0,0 +1,7 @@
+package com.example.cvproject.repository;
+
+import com.example.cvproject.model.SkillsModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SkillsRepository extends JpaRepository<SkillsModel, Long> {
+}

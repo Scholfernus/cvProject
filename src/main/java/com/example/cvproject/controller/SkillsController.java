4888a288b36edf1f46e07fc7ccfcebfca8cc7d50
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-09 22:01:12 +0100

    SkillsController - utworzenie metod w kontroller

--- src/main/java/com/example/cvproject/controller/SkillsController.java
+++ src/main/java/com/example/cvproject/controller/SkillsController.java
@@ -1,15 +1,24 @@
 package com.example.cvproject.controller;
 
+import com.example.cvproject.model.SkillsModel;
+import com.example.cvproject.repository.SkillsRepository;
+import com.example.cvproject.service.SkillsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/skills")
 public class SkillsController {
-
+private SkillsService skillsService;
     @GetMapping
     public String getSkills(){
         return "skills/skills";
+    }
+    @PostMapping("/addSkill")
+    public String addSkill(SkillsModel model){
+    skillsService.addSkillToList(model);
+    return "skills/skills";
     }
 }

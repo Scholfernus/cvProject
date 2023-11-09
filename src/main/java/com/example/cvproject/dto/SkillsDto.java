729ca6b7fd865a3f326ce2823660a063a5706371
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-09 22:01:40 +0100

    Utworzenie DTO w controller

---
+++ src/main/java/com/example/cvproject/dto/SkillsDto.java
@@ -0,0 +1,10 @@
+package com.example.cvproject.dto;
+
+import lombok.Data;
+
+@Data
+public class SkillsDto {
+    private String skill;
+    private String description;
+    private String achievement;
+}

485dfebc7992b1c17964ed47b964d2a4bf50d4a0
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-09 22:05:49 +0100

    Usuwanie skilll

--- src/main/java/com/example/cvproject/controller/SkillsController.java
+++ src/main/java/com/example/cvproject/controller/SkillsController.java
@@ -5,8 +5,10 @@ import com.example.cvproject.repository.SkillsRepository;
 import com.example.cvproject.service.SkillsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping(value = "/skills")
@@ -20,5 +22,10 @@ private SkillsService skillsService;
     public String addSkill(SkillsModel model){
     skillsService.addSkillToList(model);
     return "skills/skills";
+    }
+    @PostMapping("/del/{id}")
+    public RedirectView deleteSkillById(@PathVariable("id") Long id){
+        skillsService.deleteById(id);
+        return new RedirectView("skills/skills");
     }
 }

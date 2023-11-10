a05d07fc348324b8b90ede5c5b4493e058b6aa1a
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-10 22:06:10 +0100

    W klasie modelowej dodałem @Data

--- src/main/java/com/example/cvproject/model/SkillsModel.java
+++ src/main/java/com/example/cvproject/model/SkillsModel.java
@@ -1,5 +1,7 @@
 package com.example.cvproject.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,6 +9,7 @@ import javax.persistence.Id;
 import javax.swing.*;
 
 @Entity
+@Data
 public class SkillsModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

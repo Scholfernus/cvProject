e0cfc19f092401491944da31f1d43db33ec4c96d
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-18 22:23:07 +0100

    Zmiany w kontrolerze - dadano z chata

--- src/main/java/com/example/cvproject/controller/SkillsController.java
+++ src/main/java/com/example/cvproject/controller/SkillsController.java
@@ -3,21 +3,14 @@ package com.example.cvproject.controller;
 import com.example.cvproject.model.SkillsModel;
 import com.example.cvproject.service.SkillsService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/skills")
+@RequestMapping("/skills")
 @Slf4j
 public class SkillsController {
-    private static final Logger logger = LoggerFactory.getLogger(SkillsController.class);
     private final SkillsService skillsService;
 
     public SkillsController(SkillsService skillsService) {
@@ -25,37 +18,35 @@ public class SkillsController {
     }
 
     @GetMapping
-    public String getSkills() {
-        return "skills/skills";
+    @ResponseBody
+    public ModelAndView getSkills() {
+        return new ModelAndView("skills/skills");
     }
     @GetMapping("/add")
-    public String getAddSkill(Model model) {
-        logger.debug("Entering getAddSkill method");
+    @ResponseBody
+    public ModelAndView getAddSkill() {
+        log.debug("Entering getAddSkill method");
         skillsService.getAddSkill();
-        model.addAttribute("someAttribute", "someValue");
+        ModelAndView mav = new ModelAndView("skills/addSkill");
+        mav.addObject("someAttribute", "someValue");
         // Zwróć widok, który ma obsłużyć formularz dodawania umiejętności
-        return "skills/addSkill";
+        return mav;
     }
     @PostMapping("/addSkill")
     public String addSkill(SkillsModel model) {
         skillsService.addSkillToList(model);
-        return "skills/skills";
+        return "redirect:skills/skills";
     }
-//    @GetMapping("/add")
-//    public String getAddSkill(){
-//        skillsService.getAddSkill();
-//        return "skills/addSkill";
-//    }
 
     @PostMapping("/del/{id}")
-    public RedirectView deleteSkillById(@PathVariable("id") Long id) {
+    public String deleteSkillById(@PathVariable("id") Long id) {
         skillsService.deleteById(id);
-        return new RedirectView("skills/skills");
+        return "redirect:skills/skills";
     }
 
     @PostMapping("/edit/{id}")
-    public RedirectView editSkillById(@PathVariable("id") Long id, SkillsModel model) {
+    public String editSkillById(@PathVariable("id") Long id, SkillsModel model) {
         skillsService.editSkillById(model, id);
-        return new RedirectView("skills/skills");
+        return "redirect:skills/skills";
     }
 }

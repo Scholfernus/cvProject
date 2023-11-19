935e8e75299f8b5f7a31bdf510ce250106fd6bde
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-19 15:36:51 +0100

    Wstrzykiwanie przez Constructor

---
+++ src/main/java/com/example/cvproject/DummyLogger/CommandLineRunnerWithConstructorInjection.java
@@ -0,0 +1,15 @@
+package com.example.cvproject.DummyLogger;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CommandLineRunnerWithConstructorInjection implements CommandLineRunner {
+    private final DummyLogger logger;
+    @Override
+    public void run(String... args) throws Exception {
+     logger.sayHello();
+    }
+}

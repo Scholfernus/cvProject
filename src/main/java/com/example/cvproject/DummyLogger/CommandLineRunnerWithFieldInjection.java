8a4d2d3c36b23393552d708c63d204f54e59b35f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-19 15:37:15 +0100

    Wstrzykiwanie przez pole

---
+++ src/main/java/com/example/cvproject/DummyLogger/CommandLineRunnerWithFieldInjection.java
@@ -0,0 +1,15 @@
+package com.example.cvproject.DummyLogger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineRunnerWithFieldInjection implements CommandLineRunner {
+    @Autowired
+    private DummyLogger dummyLogger;
+    @Override
+    public void run(String... args) throws Exception {
+    dummyLogger.sayHello();
+    }
+}

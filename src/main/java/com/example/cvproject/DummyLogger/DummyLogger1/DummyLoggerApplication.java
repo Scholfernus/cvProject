64fa073d2fdb9fdba2a52010b43108e2f11e0ad2
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-20 22:05:00 +0100

    DummyLoggerApplication main

--- src/main/java/com/example/cvproject/DummyLogger/DummyLogger1/DummyLoggerApplication.java
+++ src/main/java/com/example/cvproject/DummyLogger/DummyLogger1/DummyLoggerApplication.java
@@ -1,4 +1,4 @@
-package com.example.cvproject.DummyLogger;
+package com.example.cvproject.DummyLogger.DummyLogger1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

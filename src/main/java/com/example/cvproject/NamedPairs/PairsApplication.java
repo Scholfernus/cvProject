9f45db8c00a6dbad8563555229fa808b2be0889f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-20 22:06:35 +0100

    PairsApplication klasa main

---
+++ src/main/java/com/example/cvproject/NamedPairs/PairsApplication.java
@@ -0,0 +1,11 @@
+package com.example.cvproject.NamedPairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PairsApplication.class, args);
+    }
+}

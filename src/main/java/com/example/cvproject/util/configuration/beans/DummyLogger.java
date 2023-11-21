240d76486ff23d9c32d435bd99aeb97f1225ce9a
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-21 21:38:39 +0100

    DammyLogger - pola klasy

---
+++ src/main/java/com/example/cvproject/util/configuration/beans/DummyLogger.java
@@ -0,0 +1,10 @@
+package com.example.cvproject.util.configuration.beans;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DummyLogger {
+private void sayHi(){
+    log.info("Hi from zadanie 4");
+}
+}

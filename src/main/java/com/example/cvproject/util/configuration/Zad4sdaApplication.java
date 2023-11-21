0bbd983eee0aae9fbb9df0bf43a994d1879d0c0f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-21 21:35:16 +0100

    Zad4sdaApplication klasa main

---
+++ src/main/java/com/example/cvproject/util/configuration/Zad4sdaApplication.java
@@ -0,0 +1,11 @@
+package com.example.cvproject.util.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad4sdaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Zad4sdaApplication.class,args);
+    }
+}

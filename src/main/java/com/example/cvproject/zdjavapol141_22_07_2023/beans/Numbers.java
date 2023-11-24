92b67ea93dc45a387ad369b6de27096b7e0c58f5
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-24 22:26:29 +0100

    Bean  w pakiecie Bean

---
+++ src/main/java/com/example/cvproject/zdjavapol141_22_07_2023/beans/Numbers.java
@@ -0,0 +1,11 @@
+package com.example.cvproject.zdjavapol141_22_07_2023.beans;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class Numbers {
+
+private int number = 10;
+}

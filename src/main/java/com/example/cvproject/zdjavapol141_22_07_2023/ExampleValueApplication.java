207ee2983cb8c26844fdd44b4a1d6a7ad173f03a
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-24 22:04:59 +0100

    Klasa startowa

---
+++ src/main/java/com/example/cvproject/zdjavapol141_22_07_2023/ExampleValueApplication.java
@@ -0,0 +1,12 @@
+package com.example.cvproject.zdjavapol141_22_07_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleValueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleValueApplication.class,args);
+    }
+
+}

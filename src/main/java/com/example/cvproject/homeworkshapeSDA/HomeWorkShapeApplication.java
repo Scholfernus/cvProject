650754e660fd48c7b00829add02051880a6632c9
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-01 12:31:44 +0100

    Klasa startowa

---
+++ src/main/java/com/example/cvproject/homeworkshapeSDA/HomeWorkShapeApplication.java
@@ -0,0 +1,11 @@
+package com.example.cvproject.homeworkshapeSDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkShapeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkShapeApplication.class, args);
+    }
+}

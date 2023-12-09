29c8b680e1d34c87ebf18bc1284dfec7a5dcab4f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-09 21:14:44 +0100

    Zadanie 9 - DTO

---
+++ src/main/java/com/example/cvproject/zdjavapol141web/zad9sda/dto/FileDataDto.java
@@ -0,0 +1,20 @@
+package com.example.cvproject.zdjavapol141web.zad9sda.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+public class FileDataDto {
+    @NotBlank
+    private String fileName;
+    @NotBlank
+    private String extension;
+    @Min(1)
+    private Integer sizeInKb;
+    @NotBlank
+    private String content;
+}
